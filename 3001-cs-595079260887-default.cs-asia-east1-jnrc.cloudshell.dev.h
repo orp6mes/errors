7bea0786e9fb18d0d67fd24dcf079344092ecb45
orp6mes <swaxx10@outlook.com>
2024-01-19 12:29:27 +0530

    Error

---
+++ 3001-cs-595079260887-default.cs-asia-east1-jnrc.cloudshell.dev.h
@@ -0,0 +1,376 @@
+{
+  "log": {
+    "version": "1.2",
+    "creator": {
+      "name": "WebInspector",
+      "version": "537.36"
+    },
+    "pages": [],
+    "entries": [
+      {
+        "_initiator": {
+          "type": "script",
+          "stack": {
+            "callFrames": [
+              {
+                "functionName": "handleSubmit",
+                "scriptId": "232",
+                "url": "webpack-internal:///(app-pages-browser)/./src/app/page.tsx",
+                "lineNumber": 22,
+                "columnNumber": 31
+              },
+              {
+                "functionName": "callCallback",
+                "scriptId": "41",
+                "url": "webpack-internal:///(app-pages-browser)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js",
+                "lineNumber": 20460,
+                "columnNumber": 13
+              },
+              {
+                "functionName": "invokeGuardedCallbackImpl",
+                "scriptId": "41",
+                "url": "webpack-internal:///(app-pages-browser)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js",
+                "lineNumber": 20509,
+                "columnNumber": 15
+              },
+              {
+                "functionName": "invokeGuardedCallback",
+                "scriptId": "41",
+                "url": "webpack-internal:///(app-pages-browser)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js",
+                "lineNumber": 20584,
+                "columnNumber": 28
+              },
+              {
+                "functionName": "invokeGuardedCallbackAndCatchFirstError",
+                "scriptId": "41",
+                "url": "webpack-internal:///(app-pages-browser)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js",
+                "lineNumber": 20598,
+                "columnNumber": 24
+              },
+              {
+                "functionName": "executeDispatch",
+                "scriptId": "41",
+                "url": "webpack-internal:///(app-pages-browser)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js",
+                "lineNumber": 31935,
+                "columnNumber": 2
+              },
+              {
+                "functionName": "processDispatchQueueItemsInOrder",
+                "scriptId": "41",
+                "url": "webpack-internal:///(app-pages-browser)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js",
+                "lineNumber": 31967,
+                "columnNumber": 6
+              },
+              {
+                "functionName": "processDispatchQueue",
+                "scriptId": "41",
+                "url": "webpack-internal:///(app-pages-browser)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js",
+                "lineNumber": 31980,
+                "columnNumber": 4
+              },
+              {
+                "functionName": "dispatchEventsForPlugins",
+                "scriptId": "41",
+                "url": "webpack-internal:///(app-pages-browser)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js",
+                "lineNumber": 31991,
+                "columnNumber": 2
+              },
+              {
+                "functionName": "eval",
+                "scriptId": "41",
+                "url": "webpack-internal:///(app-pages-browser)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js",
+                "lineNumber": 32181,
+                "columnNumber": 11
+              },
+              {
+                "functionName": "batchedUpdates$1",
+                "scriptId": "41",
+                "url": "webpack-internal:///(app-pages-browser)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js",
+                "lineNumber": 24792,
+                "columnNumber": 11
+              },
+              {
+                "functionName": "batchedUpdates",
+                "scriptId": "41",
+                "url": "webpack-internal:///(app-pages-browser)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js",
+                "lineNumber": 28652,
+                "columnNumber": 11
+              },
+              {
+                "functionName": "dispatchEventForPluginEventSystem",
+                "scriptId": "41",
+                "url": "webpack-internal:///(app-pages-browser)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js",
+                "lineNumber": 32180,
+                "columnNumber": 2
+              },
+              {
+                "functionName": "dispatchEvent",
+                "scriptId": "41",
+                "url": "webpack-internal:///(app-pages-browser)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js",
+                "lineNumber": 29948,
+                "columnNumber": 4
+              },
+              {
+                "functionName": "dispatchDiscreteEvent",
+                "scriptId": "41",
+                "url": "webpack-internal:///(app-pages-browser)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js",
+                "lineNumber": 29919,
+                "columnNumber": 4
+              }
+            ]
+          }
+        },
+        "_priority": "High",
+        "_resourceType": "fetch",
+        "cache": {},
+        "request": {
+          "method": "POST",
+          "url": "https://5328-cs-595079260887-default.cs-asia-east1-jnrc.cloudshell.dev/dashfilter",
+          "httpVersion": "HTTP/1.1",
+          "headers": [
+            {
+              "name": "Accept",
+              "value": "*/*"
+            },
+            {
+              "name": "Accept-Encoding",
+              "value": "gzip, deflate, br"
+            },
+            {
+              "name": "Accept-Language",
+              "value": "en-US,en;q=0.9"
+            },
+            {
+              "name": "Connection",
+              "value": "keep-alive"
+            },
+            {
+              "name": "Content-Length",
+              "value": "78"
+            },
+            {
+              "name": "Content-Type",
+              "value": "application/json"
+            },
+            {
+              "name": "Host",
+              "value": "5328-cs-595079260887-default.cs-asia-east1-jnrc.cloudshell.dev"
+            },
+            {
+              "name": "Origin",
+              "value": "https://3001-cs-595079260887-default.cs-asia-east1-jnrc.cloudshell.dev"
+            },
+            {
+              "name": "Referer",
+              "value": "https://3001-cs-595079260887-default.cs-asia-east1-jnrc.cloudshell.dev/"
+            },
+            {
+              "name": "Sec-Fetch-Dest",
+              "value": "empty"
+            },
+            {
+              "name": "Sec-Fetch-Mode",
+              "value": "cors"
+            },
+            {
+              "name": "Sec-Fetch-Site",
+              "value": "same-site"
+            },
+            {
+              "name": "User-Agent",
+              "value": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0"
+            },
+            {
+              "name": "sec-ch-ua",
+              "value": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Microsoft Edge\";v=\"120\""
+            },
+            {
+              "name": "sec-ch-ua-mobile",
+              "value": "?0"
+            },
+            {
+              "name": "sec-ch-ua-platform",
+              "value": "\"Windows\""
+            }
+          ],
+          "queryString": [],
+          "cookies": [],
+          "headersSize": 759,
+          "bodySize": 78,
+          "postData": {
+            "mimeType": "application/json",
+            "text": "{\"investmentPeriod\":\"3M\",\"capitalAmount\":9999999999,\"tradeType\":\"Derivatives\"}"
+          }
+        },
+        "response": {
+          "status": 302,
+          "statusText": "Found",
+          "httpVersion": "HTTP/1.1",
+          "headers": [
+            {
+              "name": "Content-Length",
+              "value": "0"
+            },
+            {
+              "name": "Date",
+              "value": "Fri, 19 Jan 2024 06:48:55 GMT"
+            },
+            {
+              "name": "Location",
+              "value": "https://accounts.google.com/o/oauth2/v2/auth?client_id=618104708054-m0mqlm35l2ahieavnib6emtan2k95ps9.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fssh.cloud.google.com%2Fdevshell%2Fgateway%2Foauth&response_type=code&scope=email&state=eyJ0b2tlbiI6InVxRkNBODFwRzJ5UGFxTEJIa04xRHciLCJ0YXJnZXRfaG9zdCI6IjUzMjgtY3MtNTk1MDc5MjYwODg3LWRlZmF1bHQuY3MtYXNpYS1lYXN0MS1qbnJjLmNsb3Vkc2hlbGwuZGV2IiwiYXV0aHVzZXIiOiIifQ"
+            }
+          ],
+          "cookies": [],
+          "content": {
+            "size": 0,
+            "mimeType": "x-unknown",
+            "compression": 502
+          },
+          "redirectURL": "https://accounts.google.com/o/oauth2/v2/auth?client_id=618104708054-m0mqlm35l2ahieavnib6emtan2k95ps9.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fssh.cloud.google.com%2Fdevshell%2Fgateway%2Foauth&response_type=code&scope=email&state=eyJ0b2tlbiI6InVxRkNBODFwRzJ5UGFxTEJIa04xRHciLCJ0YXJnZXRfaG9zdCI6IjUzMjgtY3MtNTk1MDc5MjYwODg3LWRlZmF1bHQuY3MtYXNpYS1lYXN0MS1qbnJjLmNsb3Vkc2hlbGwuZGV2IiwiYXV0aHVzZXIiOiIifQ",
+          "headersSize": 502,
+          "bodySize": -502,
+          "_transferSize": 0,
+          "_error": "net::ERR_FAILED"
+        },
+        "serverIPAddress": "",
+        "startedDateTime": "2024-01-19T06:48:51.597Z",
+        "time": 477.56700000172714,
+        "timings": {
+          "blocked": 477.56700000172714,
+          "dns": -1,
+          "ssl": -1,
+          "connect": -1,
+          "send": 0,
+          "wait": 0,
+          "receive": 0,
+          "_blocked_queueing": 364.17100000107894
+        }
+      },
+      {
+        "_initiator": {
+          "type": "preflight",
+          "url": "https://5328-cs-595079260887-default.cs-asia-east1-jnrc.cloudshell.dev/dashfilter",
+          "requestId": "8412.201"
+        },
+        "_priority": "High",
+        "_resourceType": "preflight",
+        "cache": {},
+        "connection": "143499",
+        "request": {
+          "method": "OPTIONS",
+          "url": "https://5328-cs-595079260887-default.cs-asia-east1-jnrc.cloudshell.dev/dashfilter",
+          "httpVersion": "HTTP/1.1",
+          "headers": [
+            {
+              "name": "Accept",
+              "value": "*/*"
+            },
+            {
+              "name": "Accept-Encoding",
+              "value": "gzip, deflate, br"
+            },
+            {
+              "name": "Accept-Language",
+              "value": "en-US,en;q=0.9"
+            },
+            {
+              "name": "Access-Control-Request-Headers",
+              "value": "content-type"
+            },
+            {
+              "name": "Access-Control-Request-Method",
+              "value": "POST"
+            },
+            {
+              "name": "Connection",
+              "value": "keep-alive"
+            },
+            {
+              "name": "Host",
+              "value": "5328-cs-595079260887-default.cs-asia-east1-jnrc.cloudshell.dev"
+            },
+            {
+              "name": "Origin",
+              "value": "https://3001-cs-595079260887-default.cs-asia-east1-jnrc.cloudshell.dev"
+            },
+            {
+              "name": "Referer",
+              "value": "https://3001-cs-595079260887-default.cs-asia-east1-jnrc.cloudshell.dev/"
+            },
+            {
+              "name": "Sec-Fetch-Dest",
+              "value": "empty"
+            },
+            {
+              "name": "Sec-Fetch-Mode",
+              "value": "cors"
+            },
+            {
+              "name": "Sec-Fetch-Site",
+              "value": "same-site"
+            },
+            {
+              "name": "User-Agent",
+              "value": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0"
+            }
+          ],
+          "queryString": [],
+          "cookies": [],
+          "headersSize": 662,
+          "bodySize": 0
+        },
+        "response": {
+          "status": 200,
+          "statusText": "OK",
+          "httpVersion": "HTTP/1.1",
+          "headers": [
+            {
+              "name": "Access-Control-Allow-Headers",
+              "value": "content-type"
+            },
+            {
+              "name": "Access-Control-Allow-Methods",
+              "value": "GET,OPTIONS"
+            },
+            {
+              "name": "Access-Control-Allow-Origin",
+              "value": "https://3001-cs-595079260887-default.cs-asia-east1-jnrc.cloudshell.dev"
+            },
+            {
+              "name": "Content-Length",
+              "value": "0"
+            },
+            {
+              "name": "Date",
+              "value": "Fri, 19 Jan 2024 06:48:55 GMT"
+            }
+          ],
+          "cookies": [],
+          "content": {
+            "size": 0,
+            "mimeType": "x-unknown",
+            "compression": 263
+          },
+          "redirectURL": "",
+          "headersSize": 263,
+          "bodySize": -263,
+          "_transferSize": 0,
+          "_error": null
+        },
+        "serverIPAddress": "35.229.195.105",
+        "startedDateTime": "2024-01-19T06:48:51.598Z",
+        "time": 362.93400000046097,
+        "timings": {
+          "blocked": -0.6839999999999999,
+          "dns": 0.0050000000000000044,
+          "ssl": 114.815,
+          "connect": 224.816,
+          "send": 0.17499999999998295,
+          "wait": 137.53400000193577,
+          "receive": 0.4039999985252507,
+          "_blocked_queueing": -1
+        }
+      }
+    ]
+  }
+}
